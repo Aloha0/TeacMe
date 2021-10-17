b16f189e0ae83e9c2ae355b3106db772dc8fcba7
Aloha0 <stanulevich97@gmail.com>
2021-10-17 11:12:54 +0300

    first

---
+++ Test3.py
@@ -0,0 +1,79 @@
+#work 2
+
+#name = input('предложение из двух слов')
+#name_list = name.split(' ')  #разделяет на знаки
+#name_list.reverse() 
+#name = ' '.join(name_list) # 
+#print('!' + name + '!')
+
+
+
+
+#Задание 3
+"""
+ a = 10 
+b = 20
+if a+b > 0 and b >25: # Если условие верно то пишет yes
+    print('yes')
+else: # Если не равно то пишет 
+    print('no') """
+########
+"""
+sa = input('Введите 3 символа')
+if len(sa)%3==0:
+    print(sa +'!')
+else:
+    print('no')
+    """
+##########
+""""
+sa = input('131231')
+if 'code' in sa: #Если в строке 'sa' есть слово code выведет yes
+    print('yes')
+else:
+    print('no')
+    """
+###########
+"""
+sa = int(input('Введите свой возраст'))
+if sa < 0:
+    print('Wrong input')
+elif sa < 18:
+    print('CoCa Cola')
+else:
+    print('Beer')
+    """
+    ### 
+    
+"""a = int(input(' '))
+b = int(input(' '))
+c = int(input(' '))
+
+sa = int((b**2) - (4*a*c))
+if sa >= 5:
+    print('lol')
+elif sa < 5:
+    print('kek')
+else:
+    print('no') """
+
+###########
+
+"""rubl = int(input('рубли '))
+kop = int(input('копейки '))
+
+if rubl == 1:
+    print(rubl, 'Рубль')
+elif rubl >= 2 and rubl < 5:
+    print(rubl, 'Рубля')
+elif rubl >= 5:
+    print(rubl, 'Рублей')
+if kop == 1:
+    print(kop, 'Копейка')
+elif kop >=2 and kop <=4:
+    print(kop, 'Копейки')
+elif kop >=5:
+    print(kop,  'Копеек')"""
+#####
+
+
